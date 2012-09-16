58c746025f9c3024ddb08aa9301973804f503d9f
Thunyathon Jaruchotrattansakul <lostseaway@me.com>
2012-09-16 18:40:02 +0700

    add logo.html/logodata.js

---
+++ js/logodata.js
@@ -0,0 +1,12 @@
+var data=[
+		{"img":"https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-ash4/423954_420010251380794_1667178180_n.jpg","by":"Natchanon Hongladaromp","re":"SKE"},
+		{"img":"https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-ash3/560452_465127890176392_430370542_n.jpg","by":"Pitchaya Myotan Es","re":"CPE"},
+		{"img":"https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-snc7/429881_4302267247095_988173282_n.jpg","by":"Peraphol Anantavarasilp","re":"CPE"},
+		{"img":"https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-snc6/s480x480/222084_442323169143117_706622990_n.jpg","by":"Sarun Wongtanakarn","re":"SKE"},
+		{"img":"https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-snc7/309282_464413303581184_1682620998_n.jpg","by":"Surote Wongpaiboon","re":"CPE"},
+		{"img":"https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-prn1/546082_280748848701658_175067235_n.jpg","by":"Art Thunder","re":"CPE"},
+		{"img":"https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-ash3/s480x480/539501_464160633606451_727445157_n.jpg","by":"Surote Wongpaiboon","re":"CPE"},
+		{"img":"https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-ash4/399003_437836799600759_295976895_n.jpg","by":"Parinya Peapanom","re":"CPE"},
+		{"img":"https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-ash3/558602_4235361134484_943078791_n.jpg","by":"Peraphol Anantavarasilp","re":"CPE"},
+		{"img":"https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-ash3/564554_522096657807564_1965766988_n.jpg","by":"Thunyathon Jaruchotrattanasakul","re":"SKE"},
+		{"img":"https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-snc6/185121_4235033446292_1785780363_n.jpg","by":"Peraphol Anantavarasilp","re":"CPE"}];
