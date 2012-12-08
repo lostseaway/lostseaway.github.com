e7737515004cc302566f456ea0b22f34c559117a
Thunyathon Jaruchotrattansakul <lostseaway@me.com>
2012-12-08 14:51:25 +0700

    updata calendar new semester

---
+++ js/cldata.js
@@ -0,0 +1,57 @@
+var data =[
+			{"sc":"1","code":"01175112","subject":"Badminton","credit":"1","time":{"day":["Mon"],"start":["08:00"],"end":["10:00"]},"place":"Sports Office","teacher":"Jutamas","sec":"450","addStart":[1],"addEnd":[4]},
+			{"sc":"1","code":"01175112","subject":"Badminton","credit":"1","time":{"day":["Tue"],"start":["08:00"],"end":["10:00"]},"place":"Sports Office","teacher":"Nattaya","sec":"451","addStart":[1],"addEnd":[4]},
+			{"sc":"1","code":"01175123","subject":"Basketball","credit":"1","time":{"day":["Mon"],"start":["08:00"],"end":["10:00"]},"place":"Sports Office","teacher":"Suwimon","sec":"450","addStart":[1],"addEnd":[4]},
+			{"sc":"1","code":"01175143","subject":"Social Dance","credit":"1","time":{"day":["Mon"],"start":["08:00"],"end":["10:00"]},"place":"Ed 1-301","teacher":"Ashara","sec":"450","addStart":[1],"addEnd":[4]},
+			{"sc":"1","code":"01175163","subject":"Golf","credit":"1","time":{"day":["Tue"],"start":["8:00"],"end":["10:00"]},"place":"ED 1 floor","teacher":"Dahsak","sec":"450","addStart":[1],"addEnd":[4]},
+			{"sc":"1","code":"01200111","subject":"Weight Traning","credit":"1","time":{"day":["Tue"],"start":["8:00"],"end":["10:00"]},"place":"Sports","teacher":"Phasobchok","sec":"450","addStart":[1],"addEnd":[4]},
+
+			{"sc":"2","code":"01200111","subject":"Communication Skill for Engineer","credit":"3","time":{"day":["Fri"],"start":["08:30"],"end":["12:30"]},"place":"17302","teacher":"","sec":"450","addStart":[2],"addEnd":[9]},
+			{"sc":"2","code":"01200111","subject":"Communication Skill for Engineer","credit":"3","time":{"day":["Fri"],"start":["08:30"],"end":["12:30"]},"place":"17304","teacher":"","sec":"451","addStart":[2],"addEnd":[9]},
+		
+
+			{"sc":"3","code":"01204111","subject":"Computer and Programming","credit":"3","time":{"day":["Mon"],"start":["17:00"],"end":["19:00"]},"place":"","teacher":"","sec":"450","addStart":[19],"addEnd":[22]},
+			{"sc":"3","code":"01204111","subject":"Computer and Programming Lab","credit":"3","time":{"day":["Fri"],"start":["09:00"],"end":["12:00"]},"place":"","teacher":"","sec":"450","addStart":[3],"addEnd":[8]},
+			{"sc":"3","code":"01204111","subject":"Computer and Programming Lab","credit":"3","time":{"day":[""],"start":[""],"end":[""]},"place":"E0401","teacher":"","sec":"451","addStart":[],"addEnd":[]},
+
+			{"sc":"4","code":"01219141","subject":"Object-Oriented Modeling & Programming","credit":"3","time":{"day":["Tue"],"start":["09:00"],"end":["12:00"]},"place":"E203","teacher":"James","sec":"450","addStart":[3],"addEnd":[8]},
+			{"sc":"4","code":"01219141","subject":"Object-Oriented Modeling & Programming Lab","credit":"3","time":{"day":["Wed"],"start":["13:00"],"end":["16:00"]},"place":"E201","teacher":"James","sec":"450","addStart":[11],"addEnd":[16]},
+			{"sc":"4","code":"01219141","subject":"Object-Oriented Modeling & Programming Lab","credit":"3","time":{"day":["Thu"],"start":["16:00"],"end":["19:00"]},"place":"E201","teacher":"Parut","sec":"451","addStart":[17],"addEnd":[22]},
+
+
+			{"sc":"5","code":"01403117","subject":"Fundamentals Chemistry","credit":"3","time":{"day":["Wed","Thu"],"start":["08:30","08:30"],"end":["10:30","10:30"]},"place":"17201","teacher":"","sec":"450","addStart":[2,2],"addEnd":[5,5]},
+			{"sc":"5","code":"01403117","subject":"Fundamentals Chemistry","credit":"3","time":{"day":["Wed","Thu"],"start":["08:30","08:30"],"end":["10:30","10:30"]},"place":"17302","teacher":"","sec":"451","addStart":[2,2],"addEnd":[5,5]},
+			{"sc":"5","code":"01403117","subject":"Fundamentals Chemistry","credit":"3","time":{"day":["Wed","Thu"],"start":["08:30","08:30"],"end":["10:30","10:30"]},"place":"17304","teacher":"","sec":"452","addStart":[2,2],"addEnd":[5,5]},
+			
+
+			{"sc":"6","code":"01403114","subject":"Fundamentals Chemistry Laboratory","credit":"1","time":{"day":["Tue"],"start":["16:30"],"end":["19:30"]},"place":"","teacher":"","sec":"450","addStart":[18],"addEnd":[21]},
+			{"sc":"6","code":"01403114","subject":"Fundamentals Chemistry Laboratory","credit":"1","time":{"day":["Tue"],"start":["16:30"],"end":["19:30"]},"place":"","teacher":"","sec":"451","addStart":[18],"addEnd":[21]},
+			{"sc":"6","code":"01403114","subject":"Fundamentals Chemistry Laboratory","credit":"1","time":{"day":["Thu"],"start":["17:00"],"end":["20:00"]},"place":"","teacher":"","sec":"452","addStart":[19],"addEnd":[22]},
+			{"sc":"6","code":"01403114","subject":"Fundamentals Chemistry Laboratory","credit":"1","time":{"day":["Thu"],"start":["17:00"],"end":["20:00"]},"place":"","teacher":"","sec":"453","addStart":[19],"addEnd":[22]},
+			{"sc":"6","code":"01403114","subject":"Fundamentals Chemistry Laboratory","credit":"1","time":{"day":["Thu"],"start":["17:00"],"end":["20:00"]},"place":"","teacher":"","sec":"454","addStart":[19],"addEnd":[22]},
+			{"sc":"6","code":"01403114","subject":"Fundamentals Chemistry Laboratory","credit":"1","time":{"day":["Thu"],"start":["17:00"],"end":["20:00"]},"place":"","teacher":"","sec":"455","addStart":[19],"addEnd":[22]},
+
+			{"sc":"7","code":"01420111","subject":"General Physic I","credit":"3","time":{"day":["Mon","Thu"],"start":["15:00","15:00"],"end":["16:30","16:30"]},"place":"17302","teacher":"Nopparet","sec":"450","addStart":[15,15],"addEnd":[17,17]},
+			{"sc":"7","code":"01420111","subject":"General Physic I","credit":"3","time":{"day":["Mon","Thu"],"start":["15:00","15:00"],"end":["16:30","16:30"]},"place":"17304","teacher":"Surasak","sec":"451","addStart":[15,15],"addEnd":[17,17]},
+			
+			{"sc":"8","code":"01420112","subject":"General Physic II","credit":"3","time":{"day":["Mon","Thu"],"start":["15:00","15:00"],"end":["16:30","16:30"]},"place":"17201","teacher":"Khunrapan","sec":"450","addStart":[15,15],"addEnd":[17,17]},
+			{"sc":"8","code":"01420112","subject":"General Physic II","credit":"3","time":{"day":["Mon","Thu"],"start":["15:00","15:00"],"end":["16:30","16:30"]},"place":"17303","teacher":"Chatchawan","sec":"451","addStart":[15,15],"addEnd":[17,17]},
+
+			{"sc":"9","code":"01420114","subject":"Lab General Physic II","credit":"1","time":{"day":["Mon"],"start":["17:00"],"end":["20:00"]},"place":"","teacher":"Reanu","sec":"450","addStart":[19],"addEnd":[24]},
+			{"sc":"9","code":"01420114","subject":"Lab General Physic II","credit":"1","time":{"day":["Mon"],"start":["17:00"],"end":["20:00"]},"place":"","teacher":"Nopparet","sec":"451","addStart":[19],"addEnd":[24]},
+			{"sc":"9","code":"01420114","subject":"Lab General Physic II","credit":"1","time":{"day":["Tue"],"start":["13:00"],"end":["16:00"]},"place":"","teacher":"Reanu","sec":"452","addStart":[11],"addEnd":[16]},
+			{"sc":"9","code":"01420114","subject":"Lab General Physic II","credit":"1","time":{"day":["Tue"],"start":["13:00"],"end":["16:00"]},"place":"","teacher":"Khunrapan","sec":"453","addStart":[11],"addEnd":[16]},
+			{"sc":"9","code":"01420114","subject":"Lab General Physic II","credit":"1","time":{"day":["Wed"],"start":["13:00"],"end":["16:00"]},"place":"","teacher":"Akkachai","sec":"454","addStart":[11],"addEnd":[16]},
+			{"sc":"9","code":"01420114","subject":"Lab General Physic II","credit":"1","time":{"day":["Wed"],"start":["13:00"],"end":["16:00"]},"place":"","teacher":"Surasak","sec":"455","addStart":[11],"addEnd":[16]},
+
+
+			{"sc":"10","code":"01417167","subject":"Engineering Mathematics I","credit":"4","time":{"day":["Wed","Fri"],"start":["10:30","15:00"],"end":["12:30","17:00"]},"place":"17304","teacher":"","sec":"450","addStart":[5,15],"addEnd":[9,18]},
+			
+
+			{"sc":"10","code":"01417168","subject":"Engineering Mathematics II","credit":"3","time":{"day":["Wed","Fri"],"start":["10:30","15:00"],"end":["12:30","17:00"]},"place":"17201","teacher":"","sec":"450","addStart":[5,15],"addEnd":[9,18]},
+			{"sc":"10","code":"01417168","subject":"Engineering Mathematics II","credit":"3","time":{"day":["Wed","Fri"],"start":["10:30","15:00"],"end":["12:30","17:00"]},"place":"17303","teacher":"","sec":"451","addStart":[5,15],"addEnd":[9,18]},];
+							
+							
+							
+							
+							
